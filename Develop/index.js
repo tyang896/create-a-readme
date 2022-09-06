1ae03af20001ffb87df566c1cf0d6598f6b5951e
Talee Y <talee.yng@gmail.com>
2022-09-06 00:46:10 -0500

    Add the following changes:
    * Move all files to Develop folder except for .gitignore and README.md file
    
    * Add packages and add code to functions in index.js file
    
    * Add condition statements if no license was selected

---
+++ Develop/index.js
@@ -0,0 +1,72 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const createMarkdown = require('./utils/generateMarkdown.js');
+// TODO: Create an array of questions for user input
+const questions = ["What is your GitHub username?", "What is your email address?", "What is your project's name?", "Please write a short description of your project.", "What kind of license should your project have?", "What command should be run to install dependencies?", "What command should be run to run tests?", "What does the user need to know about using the repo?", "What does the user need to know about contributing to the repo?"];
+const [username, email, title, description, license, installation, tests, usage, contributing] = questions;
+
+// TODO: Create a function to write README file
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, createMarkdown(data), err => err ? console.error(err):console.log("Successfully wrote to README.md"));
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "username",
+            message: username
+        },
+        {
+            type: "input",
+            name: "email",
+            message: email
+        },
+        {
+            type: "input",
+            name: "title",
+            message: title
+        },
+        {
+            type: "input",
+            name: "description",
+            message: description
+
+        },
+        {
+            type: "list",
+            name: "license",
+            message: license,
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: installation
+
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: tests
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: usage
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: contributing
+        }
+    ]).then((answers) => {
+        writeToFile("README.md", answers);
+    })
+}
+
+// Function call to initialize app
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,17 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === "none"){//might need to change this to null?
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === "none"){
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === "none"){
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = (data) => {
+  //Write code here to call the license functions to get the values for the license
+  //Write code for creating the README look
   return `# ${data.title}
 
 `;
